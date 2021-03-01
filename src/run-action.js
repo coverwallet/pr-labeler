4267f641e35ffd5a5e9b09231e876f896e2e98c3
Irene <contreras.lorenzo.irene@gmail.com>
2021-03-01 14:25:07 +0100

    feat(op-554): create the size labels

--- src/run-action.js
+++ src/run-action.js
@@ -3,16 +3,11 @@ const addLabel = require('./github/add-label');
 const removeLabel = require('./github/remove-label');
 
 module.exports = async tools => {
-  const size_xs = tools.inputs.xs_max_size;
-  const size_s = tools.inputs.s_max_size;
-  const size_m = tools.inputs.m_max_size;
-  const size_l = tools.inputs.l_max_size;
-
   await Promise.all([
-    createLabelIfNotExists(tools, size_xs,'ABDEE6'),
-    createLabelIfNotExists(tools, size_s, 'CBAACB'),
-    createLabelIfNotExists(tools, size_m, 'FFAEA5'),
-    createLabelIfNotExists(tools, size_l, 'FFFFB5'),
+    createLabelIfNotExists(tools, 'size_xs','abdee6'),
+    createLabelIfNotExists(tools, 'size_s', 'cbaacb'),
+    createLabelIfNotExists(tools, 'size_m', 'ffaea5'),
+    createLabelIfNotExists(tools, 'size_l', 'ffffb5'),
   ]);
 
   //await Promise.all([
