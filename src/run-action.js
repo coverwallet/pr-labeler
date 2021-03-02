caee651d3ee69faed6a2e7178e1fcef1edbcea5f
Irene <contreras.lorenzo.irene@gmail.com>
2021-03-02 12:47:57 +0100

    feat(op-554): extract label config to a new function

--- src/run-action.js
+++ src/run-action.js
@@ -2,49 +2,51 @@ const createLabelIfNotExists = require('./github/create-label-if-not-exists');
 const addLabel = require('./github/add-label');
 const removeLabel = require('./github/remove-label');
 
-const createLabelsIfNotExists = async tools => {
-    const xs = {
-        name: 'size_xs',
-        size : tools.inputs.xs_max_size,
-        color : 'abdee6'
-    }
-    const s = {
-        name: 'size_s',
-        size : tools.inputs.s_max_size,
-        color : 'cbaacb'
-    }
-    const m = {
-        name: 'size_m',
-        size : tools.inputs.m_max_size,
-        color : 'ffaea5'
-    }
-    const l = {
-        name: 'size_l',
-        size : tools.inputs.l_max_size,
-        color : 'ffffb5'
-    }
+const getLabelConfig = async (tools) => {
+  const labelConfig = [
+    {
+      name: 'size_xs',
+      size: tools.inputs.xs_max_size,
+      color: 'abdee6',
+    },
+    {
+      name: 'size_s',
+      size: tools.inputs.s_max_size,
+      color: 'cbaacb',
+    },
+    {
+      name: 'size_m',
+      size: tools.inputs.m_max_size,
+      color: 'ffaea5',
+    },
+    {
+      name: 'size_l',
+      size: tools.inputs.l_max_size,
+      color: 'ffffb5',
+    },
+  ];
+  return labelConfig;
+};
 
-    await Promise.all([
-        createLabelIfNotExists(tools, xs.name, xs),
-        createLabelIfNotExists(tools, s.name, s),
-        createLabelIfNotExists(tools, m.name, m),
-        createLabelIfNotExists(tools, l.name, l),
-    ]);
-    await Promise.all([
-        addLabel(tools, xs.name)
-    ]);
-}
+const createLabelsIfNotExists = async (tools, labelConfig) => {
+  await Promise.all(
+    labelConfig.map((item) =>
+      createLabelIfNotExists(tools, item.name, { color: item.color }),
+    ),
+  );
+};
 
-const getNumberOfLines = async tools => {
-    return 10;
-}
+const getNumberOfLines = async (tools) => {
+  return 10;
+};
 
-const assignLabelForLineChanges = async (tools, numberOfLines) => {
-    return null;
-}
+const assignLabelForLineChanges = async (tools, numberOfLines, labelConfig) => {
+  return null;
+};
 
-module.exports = async tools => {
-    await createLabelsIfNotExists(tools);
-    const numberOfLines = await getNumberOfLines(tools);
-    await assignLabelForLineChanges(tools, numberOfLines);
+module.exports = async (tools) => {
+  const labelConfig = getLabelConfig(tools);
+  await createLabelsIfNotExists(tools, labelConfig);
+  const numberOfLines = await getNumberOfLines(tools);
+  await assignLabelForLineChanges(tools, numberOfLines, labelConfig);
 };
