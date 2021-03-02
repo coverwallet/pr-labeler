7bd6774aafea4c34a025507603a7747f6a27d4b6
Irene <contreras.lorenzo.irene@gmail.com>
2021-03-02 10:51:46 +0100

    feat(op-554): add label to a pr

--- src/run-action.js
+++ src/run-action.js
@@ -4,29 +4,33 @@ const removeLabel = require('./github/remove-label');
 
 module.exports = async tools => {
     const xs = {
+        name: 'size_xs',
         size : tools.inputs.xs_max_size,
         color : 'abdee6'
     }
-
     const s = {
+        name: 'size_s',
         size : tools.inputs.s_max_size,
         color : 'cbaacb'
     }
-
     const m = {
+        name: 'size_m',
         size : tools.inputs.m_max_size,
         color : 'ffaea5'
     }
-
     const l = {
+        name: 'size_l',
         size : tools.inputs.l_max_size,
         color : 'ffffb5'
     }
 
     await Promise.all([
-        createLabelIfNotExists(tools, 'size_xs', xs),
-        createLabelIfNotExists(tools, 'size_s', s),
-        createLabelIfNotExists(tools, 'size_m', m),
-        createLabelIfNotExists(tools, 'size_l', l),
+        createLabelIfNotExists(tools, xs.name, xs),
+        createLabelIfNotExists(tools, s.name, s),
+        createLabelIfNotExists(tools, m.name, m),
+        createLabelIfNotExists(tools, l.name, l),
+    ]);
+    await Promise.all([
+        addLabel(tools, xs.name)
     ]);
 };
