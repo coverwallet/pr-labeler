bf27e7bb478968571058b0adc28ce3db249e5d86
MokkeMeguru <meguru.mokke@gmail.com>
2021-12-23 15:03:13 +0900

    feat: wip

--- src/github/exists-label.js
+++ src/github/exists-label.js
@@ -3,30 +3,20 @@
  * @param {import('actions-toolkit').Toolkit} tools
  * @param {string} labelName
  */
-module.exports = async (tools, labelName) => {
-  try {
-    const {
-      data: labelsForRepositories,
-    } = await tools.github.paginate(tools.github.issues.listLabelsForRepo, {
-      ...tools.context.repo,
-    });
 
-    tools.log.info(`what's this??? ${labelsForRepositories}`)
- 
-    const x = await tools.github.paginate(tools.github.issues.listLabelsForRepo, {
+import {Toolkit} from "actions-toolkit";
+module.exports = async (_tools, labelName) => {
+  const tools = new Toolkit()
+  try {
+    tools.github.issues.listLabelsForRepo
+    const labelsForRepository = await tools.github.paginate(tools.github.issues.listLabelsForRepo, {
       ...tools.context.repo,
     })
 
-    tools.log.info(`waht's this??????? ${x}`)
-
-    x.forEach((labels) => {
-        labels.find((label) => {
-          if (label.name === labelName) {
-            return true;
-          }
-        });
-      })
-    return false;
+    return !!labelsForRepository.find(label => {
+      tools.log.info(`what's this ${label}`)
+      return label.name === labelName;
+    });
   } catch (error) {
     tools.log.info(
       `Error happens when we was checking labels in the repository: ${error}`,
