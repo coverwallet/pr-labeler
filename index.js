f754a875e82d7bbf993ab54eed0ab128ebfadb05
yhamano0312 <72236227+yhamano0312@users.noreply.github.com>
2022-11-18 15:36:50 +0900

    support reopen event trigger

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ Toolkit.run(
       'pull_request.opened',
       'pull_request.synchronize',
       'pull_request.ready_for_review',
+      'pull_request.reopened',
     ],
     secrets: ['GITHUB_TOKEN'],
   },
